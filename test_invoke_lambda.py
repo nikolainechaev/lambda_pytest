9443b8a26039c99c28b2a1310a96d98f3c537577
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2023-03-14 16:17:44 -0400

    it works

--- test_invoke_lambda.py
+++ test_invoke_lambda.py
@@ -3,6 +3,7 @@ from invoke_lambda import runner
 
 def test_invocation():
 
-    expected = '{"statusCode": 200, "body": "\"Hello from Lambda!\""}'
+    # expected_string = '{"statusCode": 200, "body": "\\"Hello from Lambda!\\""}'
+    expected_string = '{"statusCode": 200, "body": 300}'
     lambda_result = runner()
-    assert lambda_result == expected
+    assert lambda_result == expected_string
